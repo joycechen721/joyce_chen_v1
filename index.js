e33dd2213c56353167fba6f348ba68fbe65c2786
Joyce Chen <joyce.chen721@gmail.com>
2022-08-08 17:36:24 -0700

    created new landing page

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+$("div.collapse").hide();
+
+$("button").click(function(){
+  $("div.collapse").toggle("slide");
+});
+
+function myFunction(){
+  var x = document.getElementById("topNav");
+  if (x.className === "nav-list") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-list";
+  }
+}
