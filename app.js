3c4d36ad17dbbc320215b2e0f12e4b3f0c3d172d
Joyce Chen <joyce.chen721@gmail.com>
2022-08-23 22:31:38 -0700

    add dynamically updated blog (no database) & a compose route

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const app = express();
-const autoprefixer = require('autoprefixer')
 const ejs = require('ejs')
+const autoprefixer = require('autoprefixer')
 const postcss = require('postcss')
 
-// postcss([ autoprefixer ]).process(css).then(result => {
-//   result.warnings().forEach(warn => {
-//     console.warn(warn.toString())
-//   })
-//   console.log(result.css)
-// })
-
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+const posts = [];
 
 app.get("/", function(req, res){
   res.render('index');
@@ -21,6 +17,23 @@ app.get("/", function(req, res){
 app.get("/cs31", function(req, res){
   res.render('cs31');
 });
+
+app.get("/blog", function(req, res){
+  res.render('blog', {postsArr: posts});
+})
+
+app.get("/compose", function(req, res){
+  res.render('compose');
+})
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.title,
+    input: req.body.newPost
+  };
+  posts.push(post);
+  res.redirect("/blog");
+})
 
 app.listen(process.env.PORT || 4000, function(){
   console.log("Server is running on port 4000.");
